c940c7e771134260188d75beef0dfe310683d445
잼민 <79719602+gun2475@users.noreply.github.com>
2022-04-19 16:59:21 +0900

    Update 11866.py

--- Coding/CodingTest/baekjoon/11866.py
+++ Coding/CodingTest/baekjoon/11866.py
@@ -1,5 +1,18 @@
-n, k = map(int, input().split())
-array = [i+1 for i in range(n)]
+from collections import deque
 
-for i in array:
-    print(array.pop(k+i))
+n, k = map(int, input().split())
+array = deque([i+1 for i in range(n)])
+sorT = []
+
+while array:
+    array.rotate(-(k-1))
+    sorT.append(array.popleft())
+
+print('<', end='')
+
+for i in range(len(sorT)):
+    n = sorT[i]
+    if i == len(sorT)-1:
+        print(f'{n}>')
+    else:
+        print(f'{n}, ', end='')
